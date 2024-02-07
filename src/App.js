eb4c7533e133165d08433c711ca9539a137a583b
preet <preet.enpointe>
2024-02-07 09:03:56 +0530

    testt

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white',textAlign: 'center'}}>sports web</span>
+          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
     </div>
-
   );
 }
 
